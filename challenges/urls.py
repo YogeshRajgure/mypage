ce8be480c6dabbde2c13c2e7193e86a809801c17
YogeshRajgure <noobcoderyogesh.vraj@gmail.com>
2024-08-25 19:07:48 +0530

    formatting changes

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.index),
     path("<int:month>", views.monthly_challenge_by_num),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),
-    
+
 ]
